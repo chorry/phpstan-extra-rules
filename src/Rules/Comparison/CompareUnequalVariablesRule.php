72525e1e91e0e7fd6e1eda8795ab14bafec2f4b0
Alexander Loutsenko <alutsenko@irr.ru>
2017-07-12 18:33:20 +0300

    check for all possible conditional operators

--- src/Rules/Comparison/CompareUnequalVariablesRule.php
+++ src/Rules/Comparison/CompareUnequalVariablesRule.php
@@ -1,38 +1,50 @@
 <?php declare(strict_types = 1);
 
-namespace PHPStan\Rules\Comparison;
+namespace chr\PHPStan\Rules\Comparison;
 
 use PhpParser\Node;
 use PHPStan\Analyser\Scope;
 
 class CompareUnequalVariablesRule implements \PHPStan\Rules\Rule
 {
-	public function getNodeType(): string
-	{
-		return Node\Expr\BinaryOp::class;
-	}
+    public function getNodeType(): string
+    {
+        return Node\Expr\BinaryOp::class;
+    }
 
-	/**
-	 * @param \PhpParser\Node\Expr\BinaryOp $node
-	 * @param \PHPStan\Analyser\Scope $scope
-	 * @return string[] errors
-	 */
-	public function processNode(Node $node, Scope $scope): array
-	{
-		if (!$node instanceof Node\Expr\BinaryOp\Identical && !$node instanceof Node\Expr\BinaryOp\NotIdentical) {
-			return [];
-		}
+    /**
+     * @param \PhpParser\Node\Expr\BinaryOp $node
+     * @param \PHPStan\Analyser\Scope $scope
+     * @return string[] errors
+     */
+    public function processNode(Node $node, Scope $scope): array
+    {
+        if (!$node instanceof Node\Expr\BinaryOp\Identical
+            && !$node instanceof Node\Expr\BinaryOp\NotIdentical
+            && !$node instanceof Node\Expr\BinaryOp\GreaterOrEqual
+            && !$node instanceof Node\Expr\BinaryOp\Greater
+            && !$node instanceof Node\Expr\BinaryOp\SmallerOrEqual
+            && !$node instanceof Node\Expr\BinaryOp\Smaller
+            && !$node instanceof Node\Expr\BinaryOp\Equal
+            && !$node instanceof Node\Expr\BinaryOp\NotEqual
+        ) {
+            return [];
+        }
 
-		if ($node->left->name === $node->right->name) {
-			return [
-				sprintf(
-					'Useless comparison for same variable: $%s will always evaluate to %s.',
-					$node->left->name,
-					$node instanceof Node\Expr\BinaryOp\Identical ? 'true' : 'false'
-				),
-			];
-		}
+        if ($node->left->name === $node->right->name) {
+            return [
+                sprintf(
+                    'Useless comparison for same variable: $%s will always evaluate to %s.',
+                    $node->left->name,
+                    ($node instanceof Node\Expr\BinaryOp\Identical
+                        || $node instanceof Node\Expr\BinaryOp\Equal
+                        || $node instanceof Node\Expr\BinaryOp\SmallerOrEqual
+                        || $node instanceof Node\Expr\BinaryOp\GreaterOrEqual)
+                        ? 'true' : 'false'
+                ),
+            ];
+        }
 
-		return [];
-	}
+        return [];
+    }
 }
